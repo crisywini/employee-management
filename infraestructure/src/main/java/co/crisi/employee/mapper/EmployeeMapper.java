fd7370d737d4cd17119428437234d6202f9916aa
Cristian Sánchez <harmaharcri@hotmail.com>
2023-04-21 13:10:41 -0500

    Fixed mapper builder issues

--- infraestructure/src/main/java/co/crisi/employee/mapper/EmployeeMapper.java
+++ infraestructure/src/main/java/co/crisi/employee/mapper/EmployeeMapper.java
@@ -1,19 +1,38 @@
 package co.crisi.employee.mapper;
 
+import co.crisi.employee.data.Date;
 import co.crisi.employee.data.EmployeeInfo;
 import co.crisi.employee.model.Employee;
+import java.time.LocalDate;
 import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface EmployeeMapper {
 
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "dateOfAdmission", source = "admissionDate")
     EmployeeInfo mapToInfo(Employee employee);
 
+
+    @Mapping(target = "admissionDate", source = "dateOfAdmission")
     Employee mapToEntity(EmployeeInfo employeeInfo);
 
     List<Employee> mapToEntity(List<EmployeeInfo> employeesInfo);
 
     List<EmployeeInfo> mapToInfo(List<Employee> employees);
+
+    default Date mapToDate(LocalDate localDate) {
+        return Date.builder()
+                .day(localDate.getDayOfMonth())
+                .month(localDate.getMonthValue())
+                .year(localDate.getYear())
+                .build();
+    }
+
+    default LocalDate mapToLocalDate(Date date) {
+        return LocalDate.of(date.getYear(), date.getMonth(), date.getDay());
+    }
 
 }
